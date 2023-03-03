79228200e8a6bbc96a2087a1f0effdf0c4ee4f80
Emmanuel Bergerat <45499891+gopher194@users.noreply.github.com>
2023-03-02 22:46:10 -0500

    Adapted to ImplicitUsings = false

--- PointerMover/Forms/MainForm.Designer.cs
+++ PointerMover/Forms/MainForm.Designer.cs
@@ -1,4 +1,6 @@
-﻿namespace CursorMover
+﻿using System.Windows.Forms;
+
+namespace CursorMover
 {
     partial class MainForm
     {
--- PointerMover/Program.cs
+++ PointerMover/Program.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Windows.Forms;
+
 namespace CursorMover
 {
     internal static class Program
